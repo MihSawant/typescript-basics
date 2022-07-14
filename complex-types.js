7d3eb04628f809e3ec68e78cd32c10206053689a
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-15 00:05:52 +0530

    some complex data types like array and object.

---
+++ complex-types.js
@@ -0,0 +1,17 @@
+var lagnuages;
+lagnuages = ['Java', 'JavaScript', 'TypeScript', 'Python'];
+// Default Type is any
+var jsonObject;
+jsonObject = {
+    'name': 'Mihir',
+    'age': 20,
+    'dept': 'Development'
+};
+// Object Type Def of TS
+var objectType;
+objectType = {
+    name: 'Mihir',
+    age: 20
+};
+console.log(jsonObject);
+console.log(objectType);
---
+++ complex-types.ts
@@ -0,0 +1,26 @@
+let lagnuages: string[];
+
+lagnuages = ['Java', 'JavaScript', 'TypeScript', 'Python'];
+
+// Default Type is any
+
+let jsonObject;
+
+jsonObject = {
+    'name': 'Mihir',
+    'age': 20,
+    'dept': 'Development'
+};
+
+// Object Type Def of TS
+let objectType: {
+    name: string,
+    age: number
+};
+
+objectType = {
+    name: 'Mihir',
+    age: 20
+}
+console.log(jsonObject);
+console.log(objectType);
