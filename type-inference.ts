05794250efd362469655afd1cb68419857243986
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-15 00:12:20 +0530

    type-inference feature

---
+++ type-inference.ts
@@ -0,0 +1,9 @@
+// TS inferred the type of subject to string by default
+let subject = 'Computer Science';
+/*
+so here i get an error as type-script 
+tries to infer subject to be string
+as we assigned it first to string.
+*/
+// subject = 3;
+subject = 'CS';
