aad209008f62d0b67b25a33d1c98405555a0199c
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-15 02:07:56 +0530

    interfaces in typescript

--- class-interfaces.js
+++ class-interfaces.js
@@ -14,3 +14,25 @@ var Employee = /** @class */ (function () {
 }());
 var e1 = new Employee('Foo Bar', 'Manager');
 console.log(e1);
+var bmw = {
+    speed: 75,
+    distCovered: 2,
+    changeGear: function () {
+        console.log('Bmw, Changing the Gear');
+    }
+};
+bmw.changeGear();
+var Mercedes = /** @class */ (function () {
+    function Mercedes(speed, distCovered) {
+        this.speed = speed;
+        this.distCovered = distCovered;
+    }
+    Mercedes.prototype.changeGear = function () {
+        var newSpeed = this.speed + 20;
+        console.log('Changing Gear, Speed incresing from ' + this.speed + ' to: ' + newSpeed);
+    };
+    ;
+    return Mercedes;
+}());
+var mercedesCar = new Mercedes(40, 2);
+// console.log(mercedesCar.changeGear());
--- class-interfaces.ts
+++ class-interfaces.ts
@@ -18,3 +18,40 @@ class Employee {
 
 const e1: Employee = new Employee('Foo Bar', 'Manager');
 console.log(e1);
+
+interface Car {
+    speed: number;
+    distCovered: number;
+
+    changeGear: () => void;
+}
+
+let bmw: Car = {
+    speed: 75,
+    distCovered: 2,
+
+    changeGear() {
+        console.log('Bmw, Changing the Gear');
+    },
+};
+
+bmw.changeGear();
+
+class Mercedes implements Car {
+
+    speed: number;
+    distCovered: number;
+
+    constructor(speed: number, distCovered: number) {
+        this.speed = speed;
+        this.distCovered = distCovered;
+    }
+
+    changeGear() {
+        const newSpeed = this.speed + 20;
+        console.log('Changing Gear, Speed incresing from ' + this.speed + ' to: ' + newSpeed);
+    };
+}
+
+let mercedesCar = new Mercedes(40, 2);
+// console.log(mercedesCar.changeGear());
