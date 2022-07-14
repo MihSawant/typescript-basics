30bcecdd976af2c8aeb031baf8bd1bf1356fd51c
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-14 23:56:21 +0530

    some basic primitive types usage.

---
+++ basics.js
@@ -0,0 +1,13 @@
+var payment;
+payment = 4200.5;
+/*
+In typescirpt instead of type string we can
+also assign it to String or Number but
+Number is an Object and number is primitive-type.
+
+Primitives are lower-case.
+*/
+var userName = 'Mihir';
+// let id: Number = 1;
+var loggedIn = false;
+console.log('User ' + userName + ' paid: ' + payment);
---
+++ basics.ts
@@ -0,0 +1,19 @@
+let payment: number;
+
+payment = 4200.5;
+
+/* 
+In typescirpt instead of type string we can
+also assign it to String or Number but 
+Number is an Object and number is primitive-type.
+
+Primitives are lower-case.
+*/
+
+let userName: string = 'Mihir';
+// let id: Number = 1;
+
+let loggedIn: boolean = false;
+
+console.log('User ' + userName + ' paid: ' + payment);
+
